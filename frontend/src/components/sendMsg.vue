3ea1a8114c727bb957fbb8411117447e20c2a486
Kdcc-ai <583477249@qq.com>
2020-12-22 13:08:36 +0800

    发表主题

---
+++ frontend/src/components/sendMsg.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h4 class="title">AC-Form</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {},
+  methods: {
+    handleSelect() {}
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  position: relative;
+  left: -300px;
+  margin-top: 15px;
+  margin-left: 15px;
+}
+/* .home-container {
+  height: 100%;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+} */
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from './router'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
